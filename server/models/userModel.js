e157bf234979f7593d1c4ead8d66d93eb10b09a6
Sagar Singh <sagarsingh33693@gmail.com>
2025-09-10 20:40:17 +0530

    first commit

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,19 +1,21 @@
 import mongoose from "mongoose";
-const { Schema } = mongoose;  
+const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema(
   {
-    _id: {type: String, require: true},
+    _id: { type: String, required: true }, // Clerk user ID (e.g. user_xxx)
     email: { type: String, unique: true, index: true },
-    password: { type: String, select: false },
+    password: { type: String, select: false }, // unused with Clerk
     full_name: { type: String, required: true },
-    username: { type: String, unique: true, index: true },
+    username: { type: String, unique: true, index: true, sparse: true }, 
     bio: { type: String, default: "Hey there! I am using LinkUp." },
     location: { type: String, default: "" },
     profile_picture: { type: String, default: "" },
     cover_photo: { type: String, default: "" },
-    followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+
+    // Use Clerk string IDs instead of ObjectIds
+    followers: [{ type: String, ref: "User" }],
+    following: [{ type: String, ref: "User" }],
     connections: [{ type: String, ref: "User" }],
   },
   { timestamps: true, minimize: false }
